cc9d0c9c460997c1a561dc7ff5b86b2f1fffbb25
izyak <lbackup887@gmail.com>
2022-02-26 16:39:05 +0545

    alt property added

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -7,7 +7,7 @@ import logo from "../assets/logo_transparent.png";
 const LeftBar = () => {
   return (
     <>
-    <Link to="/"><img src={logo} className={styles.smallerLogo} /></Link>
+    <Link to="/"><img  alt="hood protocol logo" src={logo} className={styles.smallerLogo} /></Link>
     <ListGroup className={styles.extraTopMargin}>
         <Link to="/"> <p className={styles.hoverOver}>Home</p></Link>
         <Link to="/reward"> <p className={styles.hoverOver}>Rewards</p></Link>
