31214cb69ddbf660aba5653dbf4510d1d534f229
izyak <lbackup887@gmail.com>
2022-02-26 16:40:20 +0545

    Overview and userOverview implemented

--- frontend/src/components/Overview.jsx
+++ frontend/src/components/Overview.jsx
@@ -3,12 +3,11 @@ import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import styles from "../styles/App.module.css";
 import { hexToExa } from "../helpers/hexToExa";
-import { ethers } from "hardhat";
+import { ethers } from "ethers";
 import { Contracts } from "../consts/Contracts";
 import DataProviderArtifact from "../contracts/lending-pool/LendingPoolDataProvider.sol/LendingPoolDataProvider.json";
 
 const Overview = () => {
-
     const [liquidityRate, setLiquidityRate] = useState(null)
     const [borrowRate, setBorrowRate] = useState(null)
     const [totalLiquidity, setTotalLiquidity] = useState(null)
@@ -16,34 +15,32 @@ const Overview = () => {
 
     useEffect(() => {
         async function fetchData() {
-            console.log('huhu')
-            // const provider = new ethers.providers.Web3Provider(window.ethereum)
-            // const dataProvider = new ethers.Contract(Contracts.dataProvider, DataProviderArtifact.abi, provider)
-            // const value = await dataProvider.getSystemLevelInfo()
-            // setLiquidityRate(hexToExa(value.liquidityRate))
-            // setTotalLiquidity(hexToExa(value.totalLiquidity))
-            // setBorrowRate(hexToExa(value.borrowRate))
-            // setTotalBorrows(hexToExa(value.totalBorrows))
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const dataProvider = new ethers.Contract(Contracts.dataProvider, DataProviderArtifact.abi, provider)
+            const value = await dataProvider.getSystemLevelInfo()
+            setLiquidityRate(hexToExa(value.liquidityRate))
+            setTotalLiquidity(hexToExa(value.totalLiquidity))
+            setBorrowRate(hexToExa(value.borrowRate))
+            setTotalBorrows(hexToExa(value.totalBorrows))
         }
         fetchData();
-    },[])
+    }, [])
     return (
         <div className={styles.centerAlign} className={styles.box}>
-            yank
-        {/* <Row>
-            <Col>
-                Total Supply <br />
-                $</Col>
-            <Col>
-                Supply APY %<br />
-                %</Col>
-            <Col>
-                Total Borrows<br />
-                $</Col>
-            <Col>
-                Borrow APY %<br />
-                %</Col>
-        </Row> */}
+            <Row>
+                <Col>
+                    Total Supply <br />
+                    ${totalLiquidity}</Col>
+                <Col>
+                    Supply APY %<br />
+                    {liquidityRate}%</Col>
+                <Col>
+                    Total Borrows<br />
+                    ${totalBorrows}</Col>
+                <Col>
+                    Borrow APY %<br />
+                    %{borrowRate}</Col>
+            </Row>
         </div>
     )
 }
--- frontend/src/components/UserOverview.jsx
+++ frontend/src/components/UserOverview.jsx
@@ -1,25 +1,50 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import styles from "../styles/App.module.css";
+import { hexToExa } from "../helpers/hexToExa";
+import { ethers } from "ethers";
+import { Contracts } from "../consts/Contracts";
+import DataProviderArtifact from "../contracts/lending-pool/LendingPoolDataProvider.sol/LendingPoolDataProvider.json";
 
-const UserOverview = ({ keyx, user, supply, supplyRate, borrow, borrowRate }) => {
+const UserOverview = ({ user }) => {
+    const [liquidityRate, setLiquidityRate] = useState(0)
+    const [borrowRate, setBorrowRate] = useState(0)
+    const [totalLiquidity, setTotalLiquidity] = useState(0)
+    const [totalBorrows, setTotalBorrows] = useState(0)
+
+    useEffect(() => {
+        async function fetchData() {
+            if (user) {
+                const provider = new ethers.providers.Web3Provider(window.ethereum)
+                const dataProvider = new ethers.Contract(Contracts.dataProvider, DataProviderArtifact.abi, provider)
+                const value = await dataProvider.getUserAccountData(user)
+                const response= await dataProvider.getSystemLevelInfo()
+                setLiquidityRate(response.liquidityRate)
+                setBorrowRate(response.borrowRate)
+                setTotalLiquidity(hexToExa(value.totalLiquidity))
+                setTotalBorrows(hexToExa(value.totalBorrows))
+            }
+        }
+        fetchData();
+    }, [user])
+    
     return (
         <div className={styles.centerAlign} className={styles.box}>
-        <Row>
-            <Col>
-                {keyx} Supply <br />
-                ${supply}</Col>
-            <Col>
-                Supply APY %<br />
-                {supplyRate}%</Col>
-            <Col>
-                {keyx} Borrows<br />
-                ${borrow}</Col>
-            <Col>
-                Borrow APY %<br />
-                {borrowRate}%</Col>
-        </Row>
+            <Row>
+                <Col>
+                    Your Supply <br />
+                    ${totalLiquidity}</Col>
+                <Col>
+                    Supply APY %<br />
+                    {liquidityRate}%</Col>
+                <Col>
+                    Your Borrows<br />
+                    ${totalBorrows}</Col>
+                <Col>
+                    Borrow APY %<br />
+                    %{borrowRate}</Col>
+            </Row>
         </div>
     )
 }
