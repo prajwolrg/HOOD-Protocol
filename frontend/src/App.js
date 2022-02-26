e20bda79410646eb60a463bce668bc26a7597011
izyak <lbackup887@gmail.com>
2022-02-26 19:23:50 +0545

    Dark Mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from "react";
-import Topbar from "./components/Navbar";
+import React, { useEffect, useState } from "react";
+// import Topbar from "./components/Navbar";
 import { ethers } from "ethers";
+import { saveToLocalStorage } from "./helpers/localStorage";
+import { formatAddress } from "./utils/formatting";
 import styles from "./styles/App.module.css"
 import Layout from "./components/Layout";
+import darkmode from "./assets/dark-mode.png";
 
 
 const App = () => {
+	const [theme, setTheme] = useState("light");
+	const [account, setAccount] = useState(null);
+
+	const connectWallet = async () => {
+		await window.ethereum.request({ method: 'eth_requestAccounts' });
+		const addr = window.ethereum.selectedAddress
+		setAccount(addr)
+		saveToLocalStorage("wallet", addr)
+	}
 
 	useEffect(() => {
 		async function fetchData() {
@@ -16,10 +28,26 @@ const App = () => {
 		fetchData();
 	}, [])
 
-	return (<div className={styles.App}>
-		<Topbar />
-		<Layout />
-	</div>)
+	const switchTheme = () => {
+		if (theme === "light") {
+			setTheme("dark")
+		} else {
+			setTheme("light")
+		}
+
+	}
+
+	return (
+		<div className={styles[`${theme}`]} >
+			<span>
+				<img onClick={switchTheme} src={darkmode} className={styles.ekdamaiSano} />
+			</span>
+			{account ? <span className={styles.rightBorder}>{formatAddress(account)} </span> :
+				<span onClick={connectWallet} className={styles.rightBorder}>
+					Connect Wallet
+				</span>}
+			<Layout />
+		</div>)
 }
 
 export default App;
