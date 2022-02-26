1bbdae04e9f79dc82cb3677c4c0441e38ca1b198
izyak <lbackup887@gmail.com>
2022-02-26 15:40:17 +0545

    Consts

---
+++ frontend/src/consts/Reservelist.js
@@ -0,0 +1,20 @@
+export const ReserveJson = [
+    {
+        "symbol": "husd",
+        "address": "0xC93716b66Fedc8b55Cbaa39ba9cBB7D5decE37b1"
+    },
+    {
+        "symbol": "hnrs",
+        "address": "0x638a7F1F9426f518607c1AFdFE0864f834573F6A"
+    },
+    {
+        "symbol": "newt",
+        "address": "0xd432Db8f0B9a5259345139a6cc012957f99e9911"
+    }
+]
+
+export const ReserveList = [
+    "0xC93716b66Fedc8b55Cbaa39ba9cBB7D5decE37b1",
+    "0x638a7F1F9426f518607c1AFdFE0864f834573F6A",
+    "0xd432Db8f0B9a5259345139a6cc012957f99e9911"
+]
