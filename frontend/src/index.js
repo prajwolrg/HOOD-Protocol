16609628b5b5309017d3f040dc6534661a6f323e
izyak <lbackup887@gmail.com>
2022-02-28 09:51:05 +0545

    Rc-Slider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-
+import 'rc-slider/assets/index.css';
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
 
