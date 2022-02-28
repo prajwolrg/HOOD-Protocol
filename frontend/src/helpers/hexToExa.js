b2ba4b3723ef2f618eda03893be0aebc646d601f
izyak <lbackup887@gmail.com>
2022-02-28 09:44:42 +0545

    Add helper function for rates

--- frontend/src/helpers/hexToExa.js
+++ frontend/src/helpers/hexToExa.js
@@ -1,3 +1,7 @@
 export function hexToExa(hexValue) {
-    return parseInt(hexValue, 16) / 10 ** 18
+    return parseInt(hexValue.toString())/10**18
+}
+
+export function hexRates(hexValue) {
+    return parseInt(hexValue.toString())/10**27
 }
