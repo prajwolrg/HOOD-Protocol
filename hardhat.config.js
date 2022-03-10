da3e76f8d88d885c1cf69bb8c4c060171b240ce2
izyak <lbackup887@gmail.com>
2022-03-10 06:36:53 +0545

    Updated hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,35 +23,18 @@ require("@nomiclabs/hardhat-waffle");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const HARMONY_PRIVATE_KEY = "257cc79f6592385715c64440661c0c5a46eb028ae787a69888cf151907ac146c";
+const PRIVATE_KEY = "257cc79f6592385715c64440661c0c5a46eb028ae787a69888cf151907ac146c";
 
 module.exports = {
   solidity: "0.5.5",
   networks: {
     matic_testnet: {
       url: `https://rpc-mumbai.maticvigil.com`,
-      accounts: [`0x${HARMONY_PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY}`]
     },
     harmony_testnet: {
       url: `https://api.s0.b.hmny.io`,
-      accounts: [`0x${HARMONY_PRIVATE_KEY}`]
-    },
-    mainnet: {
-      url: `https://api.harmony.one`,
-      accounts: [`0x${HARMONY_PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   }
 };
-
-
-// module.exports = {
-//   solidity: {
-//     version: "0.5.5",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 500
-//       }
-//     }
-//   },
-// };
