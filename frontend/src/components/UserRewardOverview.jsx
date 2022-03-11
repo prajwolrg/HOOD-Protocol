7ce4bb8f5705b3a19c1f592bbb0e40b6644a6499
izyak <lbackup887@gmail.com>
2022-03-11 06:32:29 +0545

    Claim rewards transaction

--- frontend/src/components/UserRewardOverview.jsx
+++ frontend/src/components/UserRewardOverview.jsx
@@ -5,16 +5,20 @@ import { ethers } from "ethers";
 import { Contracts } from "../consts/Contracts";
 import RewardDistributionArtifact from "../contracts/rewards/RewardDistribution.sol/RewardDistribution.json";
 import { hexToExa } from "../helpers/hexToExa";
+import Toast from "./Toast";
 import { formatDecimal } from "../utils/formatNumber";
 import styles from "../styles/App.module.css";
 
 const UserRewardOverview = ({ addr }) => {
     const [rewards, setRewards] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
             if (addr) {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const provider = new ethers.providers.Web3Provider(
+                    window.ethereum
+                );
                 const dataProvider = new ethers.Contract(
                     Contracts.reward,
                     RewardDistributionArtifact.abi,
@@ -27,14 +31,34 @@ const UserRewardOverview = ({ addr }) => {
         fetchData();
     }, [addr]);
 
+    const claimRewards = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = await provider.getSigner();
+        const rewards = new ethers.Contract(
+            Contracts.reward,
+            RewardDistributionArtifact.abi,
+            signer
+        );
+        const tx = await rewards.claimRewards();
+        const receipt = await tx.wait();
+        if (receipt.status === 1) {
+            setSuccess("true");
+        }
+    };
+
     return (
         <>
+            {success ? (
+                <Toast txn={"Rewards claimed"} success={success} />
+            ) : null}
             <div className={styles.boxCenter}>
                 Accrued Rewards <br />
                 {formatDecimal(rewards)}
             </div>
 
-            <div className={styles.button}>Claim Rewards</div>
+            <div onClick={claimRewards} className={styles.button}>
+                Claim Rewards
+            </div>
         </>
     );
 };
