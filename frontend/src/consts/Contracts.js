595333a072abf69256795eab1efee464a6c877e6
izyak <lbackup887@gmail.com>
2022-07-02 11:45:19 +0545

    update contracts

--- frontend/src/consts/Contracts.js
+++ frontend/src/consts/Contracts.js
@@ -1,3 +1,7 @@
+import Contracts from "./contractAddresses.json";
+export { Contracts };
+
+
 // matic
 // export const Contracts = {
 //   config: '0xb61B12bf15548913FAD0B22783525463465307CA',
@@ -29,23 +33,3 @@
 //   nrs: '0xb73d92F4E7C5c5aFf9f28C37C15BcC7Aef6341a3',
 //   indg: '0x4Aa96E8c1B155e20a28F528cE19Ac90243Be10d9'
 // }
-
-// hardhat node
-export const Contracts = {
-  config: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-  addressProvider: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-  lendingPoolCore: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
-  lendingPool: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-  dataProvider: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
-  initializer: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-  oracle: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
-  reward: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
-  hoodToken: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
-  usd: '0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44',
-  nrs: '0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f',
-  indg: '0x4A679253410272dd5232B3Ff7cF5dbB88f295319'
-}
-
-
-
-//   
